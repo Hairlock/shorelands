b667d5491218d210ca05819c1021c6c227dfa043
yannick <yannseal1@gmail.com>
2019-05-04 13:06:31 +0100

    w3c format for lastmod on sitemap

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,16 +1,23 @@
 const { db } = require('./database'),
     sm = require('sitemap');
 
+const lastMod = new Date().toISOString().split('T')[0];
+
 const routeUrls = [
-    { url: '', changefreq: 'yearly', priority: 0.8 },
-    { url: '/properties/homes', changefreq: 'yearly', priority: 0.9 },
-    { url: '/properties/land', changefreq: 'yearly', priority: 0.9 },
-    { url: '/properties/all', changefreq: 'yearly', priority: 0.9 }
+    { url: '', changefreq: 'yearly', priority: 0.8, lastmod: lastMod },
+    { url: '/properties/homes', changefreq: 'yearly', priority: 0.9, lastmod: lastMod },
+    { url: '/properties/land', changefreq: 'yearly', priority: 0.9, lastmod: lastMod },
+    { url: '/properties/all', changefreq: 'yearly', priority: 0.9, lastmod: lastMod }
 ]
 
 const propertyUrls = db.getData('/properties')
     .map(p => {
-        return { url: `/property/${p.slug}`, changefreq: 'weekly', priority: 1 }
+        return {
+            url: `/property/${p.slug}`,
+            changefreq: 'weekly',
+            priority: 1,
+            lastmod: lastMod
+        }
     });
 
 const sitemap = sm.createSitemap({
